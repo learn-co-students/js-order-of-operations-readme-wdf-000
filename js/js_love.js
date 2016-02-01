6c7b8a56f80136f2804953d8c538b7f98b4aca6d
Carleton DiLeo <cbdileo@gmail.com>
2016-02-01 15:49:36 -0800

    Adding finished code

--- js/js_love.js
+++ js/js_love.js
@@ -1,5 +1,10 @@
 // Functions here
 function showJsLove() {
+  console.log("I");
+  $.getJSON("https://api.github.com/repos/rails/rails/commits", function() {
+    console.log("JavaScript");
+  });
+  console.log("Love");
 };
 
 $(document).ready(function (){
