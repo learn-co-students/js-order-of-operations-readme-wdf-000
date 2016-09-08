453173dd0e4b1bd1e00f03f7e6cd05193b27e6c3
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-09-07 20:58:58 -0400

    Done.

--- js/js_love.js
+++ js/js_love.js
@@ -1,5 +1,15 @@
 // Functions here
+var url = "https://api.github.com/repos/rails/rails/commits";
+
 function showJsLove() {
+  console.log("I");
+  console.log("Love");
+  $.getJSON(url,function(data){
+    console.log("Javascript");
+    });
+    // .fail(function(error){
+    //   alert("Something went wrong: " + error);
+    // });
 };
 
 $(document).ready(function (){
