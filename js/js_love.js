430cf810acc1781a31253941bde1fa2599c0d380
Raycent <raycent.tan@flatironschool.com>
2016-09-16 03:20:47 -0400

    Done.

--- js/js_love.js
+++ js/js_love.js
@@ -1,5 +1,10 @@
 // Functions here
 function showJsLove() {
+  console.log("I");
+  $.getJSON("https://api.github.com/repos/rails/rails/commits", function(response) {
+    console.log("Javascript");
+  });
+  console.log("Love");
 };
 
 $(document).ready(function (){
