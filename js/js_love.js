1bc1ad5c8c7f0500c66213bd2208c3f8bc45e1ea
satub <satu.barnhill@gmail.com>
2016-10-05 13:57:20 -0400

    webpage/console works, first test doesn't pass

--- js/js_love.js
+++ js/js_love.js
@@ -3,6 +3,7 @@ function showJsLove() {
   console.log('I');
 
   $.getJSON('https://api.github.com/repos/rails/rails/commits', function(response){
+    // console.log(response[0]["author"]["login"])
     console.log('Javascript');
   }).fail(function(response){
     console.log('Javascript');
