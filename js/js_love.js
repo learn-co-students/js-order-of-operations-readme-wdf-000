857c076ca7db9e5b52ad27cd99638b9a666f72da
satub <satu.barnhill@gmail.com>
2016-10-05 13:59:34 -0400

    Done. Tests pass now. The first test will fail if you are trying to handle failed responses as well.

--- js/js_love.js
+++ js/js_love.js
@@ -5,9 +5,10 @@ function showJsLove() {
   $.getJSON('https://api.github.com/repos/rails/rails/commits', function(response){
     // console.log(response[0]["author"]["login"])
     console.log('Javascript');
-  }).fail(function(response){
-    console.log('Javascript');
   });
+  // .fail(function(response){
+  //   console.log('Javascript');
+  // });
     console.log('Love');
 };
 
