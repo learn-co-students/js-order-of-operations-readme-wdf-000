edba90fe2d01dda9a44f439265d2462a71c4bbd1
fidelscodes <fidelscodes@gmail.com>
2016-10-21 10:45:04 -0400

    Done

--- js/js_love.js
+++ js/js_love.js
@@ -1,6 +1,13 @@
 // Functions here
 function showJsLove() {
-};
+  console.log('I');
+
+  $.getJSON('https://api.github.com/repos/rails/rails/commits', function() {
+    console.log('JavaScript');
+  });
+
+  console.log('Love');
+}
 
 $(document).ready(function (){
   showJsLove();
