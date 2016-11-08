e7fbd4231a94100ea1a6b89b7b9eb25a5d72277d
Isuru <github email address>
2016-11-08 13:45:58 -0500

    Done.

--- js/js_love.js
+++ js/js_love.js
@@ -1,7 +1,21 @@
 // Functions here
 function showJsLove() {
+  $.getJSON("https://api.github.com/repos/rails/rails/commits", function(response){
+    console.log("I Love JavaScript");
+  })
 };
 
 $(document).ready(function (){
   showJsLove();
 });
+
+
+// console.log(1);
+// $.getJSON("https://api.github.com/repos/rails/rails/commits", function(response){
+//   // This is our success callback which is called automatically
+//   console.log("Success");
+// }).fail(function(response){
+//   // We have to explicitly configure the fail/error callback for getJSON to handle it
+//   console.log("Fail");
+// });
+// console.log(2);
