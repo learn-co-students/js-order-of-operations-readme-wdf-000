bdbdcb069501fd4b900d3fad82c3f70cb0922667
parm530 <parmsahadeo530@gmail.com>
2016-12-01 13:33:37 -0500

    DONE

--- js/js_love.js
+++ js/js_love.js
@@ -1,5 +1,12 @@
 // Functions here
 function showJsLove() {
+  console.log("I");
+
+  $.getJSON("https://api.github.com/repos/rails/rails/commits", function(response){
+    // This is our success callback which is called automatically
+    console.log("JavaScript");
+  })
+
 };
 
 $(document).ready(function (){
