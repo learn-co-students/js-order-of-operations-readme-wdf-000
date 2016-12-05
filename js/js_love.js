80a730fcbf186cc92554ac157fc4c1d70fb7f81e
jin501 <soljin01@gmail.com>
2016-12-05 18:07:18 -0500

    Done.

--- js/js_love.js
+++ js/js_love.js
@@ -1,5 +1,14 @@
 // Functions here
 function showJsLove() {
+  console.log("I")
+  $.getJSON("https://api.github.com/repos/rails/rails/commits", function(response){
+    console.log("JavaScript")
+  }).fail(function(response){
+    console.log("Fail")
+  });
+  console.log("Love")
+
+
 };
 
 $(document).ready(function (){
