01297759844cbef11a442cc1c1e26aad99c3f25a
Flatiron School <github email address>
2016-12-05 18:07:58 -0500

    done

--- js/js_love.js
+++ js/js_love.js
@@ -5,3 +5,16 @@ function showJsLove() {
 $(document).ready(function (){
   showJsLove();
 });
+
+
+function showJsLove() {
+  console.log("I");
+
+  // console.log($.get("https://api.github.com/repos/rails/rails/commits"));
+
+  $.getJSON("https://api.github.com/repos/rails/rails/commits");
+
+  console.log("Love");
+
+  console.log("Javascript");
+}
